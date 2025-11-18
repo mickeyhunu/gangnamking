a620cfef019b2ce5145baf1fbfd5e6191b061601
mickeyhunu <110079472+mickeyhunu@users.noreply.github.com>
2025-11-19 07:54:03 +0900

    Protect room map via async data feed

--- routes/entry.js
+++ routes/entry.js
@@ -2,10 +2,15 @@ const express = require('express');
 const { renderHome } = require('../controllers/entry/storeController');
 const {
   renderStoreEntries,
+  renderStoreEntriesData,
   renderStoreEntryImage,
   renderTodayImage,
 } = require('../controllers/entry/entryController');
-const { renderRoomInfo, renderRoomImage } = require('../controllers/entry/roomController');
+const {
+  renderRoomInfo,
+  renderRoomInfoData,
+  renderRoomImage,
+} = require('../controllers/entry/roomController');
 
 const router = express.Router();
 
@@ -14,8 +19,10 @@ router.get('/', (req, res) => {
 });
 
 router.get('/home', renderHome);
+router.get('/entrymap/:storeNo/data.json', renderStoreEntriesData);
 router.get('/entrymap/:storeNo', renderStoreEntries);
 router.get('/entrymap/:storeNo/entryImage', renderStoreEntryImage);
+router.get('/roommap/:storeNo/data.json', renderRoomInfoData);
 router.get('/roommap/:storeNo', renderRoomInfo);
 router.get('/roommap/:storeNo/roomImage', renderRoomImage);
 router.get('/today', renderTodayImage);
