8c09f55d4781f10292d397265e282d48c27c621a
mickeyhunu <110079472+mickeyhunu@users.noreply.github.com>
2025-11-19 09:40:51 +0900

    Add background styling to entry worker badges

--- controllers/entry/storeController.js
+++ controllers/entry/storeController.js
@@ -1,6 +1,8 @@
 const { pool } = require('../../config/db');
 const { getContentProtectionMarkup } = require('./contentProtection');
 
+const COMMUNITY_CHAT_LINK = 'https://open.kakao.com/o/gALpMlRg';
+
 async function renderHome(req, res, next) {
   try {
     const [stores] = await pool.query(
@@ -9,24 +11,29 @@ async function renderHome(req, res, next) {
         ORDER BY storeNo ASC`
     );
 
-    const protectionMarkup = getContentProtectionMarkup();
-    let html =
-      "<!DOCTYPE html><html><head><meta charset='UTF-8'><title>가게 목록</title>" +
-      protectionMarkup +
-      '</head><body>';
-    html += '<header class="community-link">강남의 밤 소통방 "강밤" : "<a href="https://open.kakao.com/o/gALpMlRg" target="_blank" rel="noopener noreferrer">https://open.kakao.com/o/gALpMlRg</a>"</header>';
-    html += '<h1>가게 목록</h1><ul>';
-    stores.forEach((store) => {
-      html += `<li>${store.storeNo} - ${store.storeName}
-                 [<a href="/entry/entrymap/${store.storeNo}">엔트리 보러가기</a>]
-                 [<a href="/entry/roommap/${store.storeNo}">룸현황 보러가기</a>]
-              </li>`;
-    });
-    html += '<li>0 - 전체 가게 [<a href="/entry/entrymap/0">엔트리 보러가기</a>] [<a href="/entry/roommap/0">룸현황 보러가기</a>]</li>';
-    html += '</ul>';
-    html += '</body></html>';
+    const storeCards = stores.map((store) => ({
+      storeNo: store.storeNo,
+      storeName: store.storeName,
+      entryUrl: `/entry/entrymap/${store.storeNo}`,
+      roomUrl: `/entry/roommap/${store.storeNo}`,
+    }));
 
-    res.send(html);
+    storeCards.push({
+      storeNo: 0,
+      storeName: '전체 가게',
+      entryUrl: '/entry/entrymap/0',
+      roomUrl: '/entry/roommap/0',
+      isAllStores: true,
+    });
+
+    res.render('entry-home', {
+      pageTitle: '가게 목록',
+      pageHeading: '가게 목록',
+      totalStores: stores.length,
+      communityLink: COMMUNITY_CHAT_LINK,
+      contentProtectionMarkup: getContentProtectionMarkup(),
+      stores: storeCards,
+    });
   } catch (error) {
     next(error);
   }
