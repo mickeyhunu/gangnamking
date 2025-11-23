169e7f9594356f8e49ce53a603b4518d680c86b3
mickeyhunu <mickeyhunu@gmail.com>
2025-11-23 10:32:56 +0900

    s

--- controllers/entry/entryController.js
+++ controllers/entry/entryController.js
@@ -817,7 +817,7 @@ async function renderStoreEntryImage(req, res, next) {
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     const referer = req.get('referer') || '직접 요청';
     const userAgent = req.get('user-agent') || '알 수 없음';
-    console.log(`[ENTRYIMAGE ACCESS] IP:${ip} REF:${referer} UA:${userAgent}`);
+    //console.log(`[ENTRYIMAGE ACCESS] IP:${ip} REF:${referer} UA:${userAgent}`);
   } catch (error) {
     next(error);
   }
