b164c3a86ee6656952b462302b299ed2b6c876f4
mickeyhunu <mickeyhunu@gmail.com>
2025-12-16 21:50:48 +0900

    a

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
-# cd Documents/aws
-# ssh -i .\room1stKey.pem ubuntu@3.39.194.110 --- cmd에서 우분투 접속
+cd Documents/aws
+ssh -i .\room1stKey.pem ubuntu@3.39.194.110 --- cmd에서 우분투 접속
 
-# sudo nano /etc/nginx/sites-available/roombbang1st
+sudo nano /etc/nginx/sites-available/roombbang1st
 
-# chmod +x ~/apps/gangnamking/deploy.sh --- sh준비
-# ~/apps/gangnamking/deploy.sh --- sh실행
-# sudo nginx -t && sudo systemctl reload nginx --- 적용
-# pm2 restart room1st --- 재실행
+chmod +x ~/apps/gangnamking/deploy.sh --- sh준비
+~/apps/gangnamking/deploy.sh --- sh실행
+sudo nginx -t && sudo systemctl reload nginx --- 적용
+pm2 restart room1st --- 재실행
 
 git fetch --all --prune
 git pull --ff-only origin "main"
